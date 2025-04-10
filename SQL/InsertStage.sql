da87081021c9064ac8bcb3eca17a62cfd6cfee0e
MundoMis <queenofreports@gmail.com>
2025-04-10 10:10:52 -0300

    Update InsertStage.sql

--- SQL/InsertStage.sql
+++ SQL/InsertStage.sql
@@ -13,9 +13,12 @@ TRUNCATE TABLE
 BULK INSERT [dbVideoGameSales].[Stage].[StgVideoGameSales]
 FROM 'C:\Users\jealu\Documents\BaseDados\vgchartz-2024.csv'
 WITH (
-    FIELDTERMINATOR = ',',  
-    ROWTERMINATOR = '0x0A', -- Para garantir que seja lido corretamente
-    FIRSTROW = 2,           
-    TABLOCK,                
-    CODEPAGE = '65001'       -- Usando UTF-8 para evitar problemas de caracteres
+	FORMAT = 'CSV',
+    FIELDTERMINATOR = ',', 
+    FIELDQUOTE = '"',              -- Aspas duplas geralmente são usadas em CSVs
+    ROWTERMINATOR = '0x0A',        -- Quebra de linha (Linux-style)
+    FIRSTROW = 2,                  -- Ignora o cabeçalho
+    TABLOCK,                       -- Otimiza a performance da carga
+    CODEPAGE = '65001'             -- UTF-8
+
 );
